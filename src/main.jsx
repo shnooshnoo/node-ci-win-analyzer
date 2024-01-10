7437bf93ba44f2c39fd4194311a4dc53fbc0a47a
Artur Yapparov <arturufa88@gmail.com>
2024-01-10 15:23:07 +0300

    123

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Index, Tests, PRs } from './routes';
+import { Index } from './routes/index.jsx';
 import './index.css';
 
 const router = createBrowserRouter([
   { path: "/", element: <Index /> },
-  { path: "/tests", element: <Tests /> },
-  { path: "/prs", element: <PRs /> },
+  {
+    path: "/tests",
+    async lazy() {
+      const { Tests } = await import("./routes/tests/tests.jsx");
+      return { Component: Tests };
+    },
+  },  {
+    path: "/prs",
+    async lazy() {
+      const { PRs } = await import("./routes/prs/pr.jsx");
+      return { Component: PRs };
+    },
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,17 +1,9 @@
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
-import { PRs } from './prs/pr.jsx';
-import { Tests } from './tests/tests.jsx';
 
-const Index = () => (
+export const Index = () => (
   <>
     <Button component={Link} to="/tests">Tests</Button>
     <Button component={Link} to="/prs">PRs</Button>
   </>
 )
-
-export {
-  Index,
-  PRs,
-  Tests
-}
