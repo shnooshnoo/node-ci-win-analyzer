68441f664426b336f681c01333c12421ce2c796e
Artur Yapparov <arturufa88@gmail.com>
2024-01-10 16:56:50 +0300

    123

--- src/common/api.js
+++ src/common/api.js
@@ -5,16 +5,16 @@ const getPRsUrl = (includeUnstable) => {
     return `http://localhost:3000/api/prs?includeUnstable=${includeUnstable}`;
   }
   if (includeUnstable) {
-    return './prs_unstable.json';
+    return './assets/prs_unstable.json';
   }
-  return './prs.json';
+  return './assets/prs.json';
 }
 
 const getTestsUrl = () => {
   if (!isProd()) {
     return 'http://localhost:3000/api/tests';
   }
-  return './tests.json';
+  return './assets/tests.json';
 }
 
 export const fetchPRs = (includeUnstable) => fetch(getPRsUrl(includeUnstable)).then((res) => res.json());
