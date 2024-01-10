ebfe96e31d7ac0e85763f2fec54534afe185cf4f
Artur Yapparov <arturufa88@gmail.com>
2024-01-10 17:03:38 +0300

    123

--- server/compileData.js
+++ server/compileData.js
@@ -5,6 +5,6 @@ const tests = getTests();
 const PRs = getPrs(false);
 const PRsUnstable = getPrs(true);
 
-fs.writeFileSync('compiledData/tests.json', JSON.stringify(tests), 'utf8');
-fs.writeFileSync('compiledData/prs.json', JSON.stringify(PRs), 'utf8');
-fs.writeFileSync('compiledData/prs_unstable.json', JSON.stringify(PRsUnstable), 'utf8');
+fs.writeFileSync('compiledData/tests.json', JSON.stringify({ data: tests }), 'utf8');
+fs.writeFileSync('compiledData/prs.json', JSON.stringify({ data: PRs }), 'utf8');
+fs.writeFileSync('compiledData/prs_unstable.json', JSON.stringify({ data: PRsUnstable }), 'utf8');
