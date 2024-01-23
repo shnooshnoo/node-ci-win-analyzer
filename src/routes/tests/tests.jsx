b0e5af5bde3b2ead12caca52434670fc0c0bf72d
Artur Yapparov <arturufa88@gmail.com>
2024-01-23 16:19:21 +0300

    123

--- src/routes/tests/tests.jsx
+++ src/routes/tests/tests.jsx
@@ -16,6 +16,7 @@ import { Source } from './source.jsx';
 
 export const Tests = () => {
   const [tests, setTests] = useState([]);
+  const [stats, setStats] = useState([]);
   const [filteredTests, setFilteredTests] = useState([]);
   const [testName, setTestName] = useState('');
   const [date, setDate] = useState('');
@@ -27,6 +28,19 @@ export const Tests = () => {
   }, []);
 
   useEffect(() => {
+    const twoWeeksAgo = Date.now() - 1000 * 60 * 60 * 24 * 14;
+    const stats = tests.reduce((acc, test) => {
+      if (test.timestamp < twoWeeksAgo) {
+        return acc;
+      }
+      return {
+        ...acc,
+        [test.testName]: acc[test.testName] ? acc[test.testName] + 1 : 1,
+      }
+    }, {});
+    const statsArr = Object.entries(stats);
+    statsArr.sort((a,b) => a[1] > b[1] ? -1 : 1)
+    setStats(statsArr.slice(0,10));
     setFilteredTests(tests.map((item) => {
       if (testName.length && !item.testName.includes(testName)) {
         return null;
@@ -48,6 +62,31 @@ export const Tests = () => {
   return (
     <>
       <Button component={Link} to="/">Back</Button>
+      <h4>Most failed tests</h4>
+      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+        <TableContainer component={Paper}>
+          <Table stickyHeader size="small" aria-label="a dense table">
+            <TableHead>
+              <TableRow>
+                <TableCell className={tableHeaderCell}>Test name</TableCell>
+                <TableCell className={tableHeaderCell}>Number of failures in the past two weeks</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {stats.map((row, index) => (
+                <TableRow
+                  key={index}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">{row[0]}</TableCell>
+                  <TableCell>{row[1]}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+      <h4>All test failures</h4>
       <div className={filterBox}>
         <TextField id="standard-basic" label="Name" variant="standard" value={testName} onChange={onTestNameChange} />
         <TextField id="standard-basic" label="Date" variant="standard" value={date} onChange={onDateChange} />
